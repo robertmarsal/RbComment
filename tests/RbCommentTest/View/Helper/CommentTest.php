f8bb3a038334cc708a197979a977739e56f09c37
Robert Boloc <robertboloc@gmail.com>
2017-01-26 21:51:53 +0000

    Fix comment view helper unit tests

--- tests/RbCommentTest/View/Helper/CommentTest.php
+++ tests/RbCommentTest/View/Helper/CommentTest.php
@@ -1,24 +1,32 @@
 <?php
-
 namespace RbCommentTest\Mvc\Controller\Plugin;
 
+use RbComment\Model\CommentTable;
 use RbComment\View\Helper\Comment as RbCommentViewHelper;
 use PHPUnit_Framework_TestCase;
 
 class CommentTest extends PHPUnit_Framework_TestCase
 {
-    public function testSetAndGetServiceLocator()
+    /**
+     * @group viewhelper
+     */
+    public function testConstructStoresTheDependencies()
     {
-        $serviceLocatorMock = $this->getMock(
-            'Zend\ServiceManager\ServiceLocatorInterface',
-            [],
-            [],
-            'ServiceLocatorInterface'
+        $viewHelperManagerMock = $this->getMockBuilder('ViewHelperManager')->getMock();
+        $routerServiceMock     = $this->getMockBuilder('Router')->getMock();
+        $configServiceMock     = [uniqid()];
+        $commentTableMock      = $this->createMock(CommentTable::class);
+
+        $commentViewHelper = new RbCommentViewHelper(
+            $viewHelperManagerMock,
+            $routerServiceMock,
+            $configServiceMock,
+            $commentTableMock
         );
 
-        $commentViewHelper = new RbCommentViewHelper();
-        $commentViewHelper->setServiceLocator($serviceLocatorMock);
-
-        $this->assertEquals($serviceLocatorMock, $commentViewHelper->getServiceLocator());
+        $this->assertAttributeEquals($viewHelperManagerMock, 'viewHelperManager', $commentViewHelper);
+        $this->assertAttributeEquals($routerServiceMock, 'routerService', $commentViewHelper);
+        $this->assertAttributeEquals($configServiceMock, 'configService', $commentViewHelper);
+        $this->assertAttributeEquals($commentTableMock, 'commentTable', $commentViewHelper);
     }
 }
